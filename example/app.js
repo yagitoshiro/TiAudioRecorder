d709b6b1065866cd10337e34620b68effdd1d513
yagi_ <yagitoshiro@gmail.com>
2012-07-24 20:44:58 +0900

    update example

--- example/app.js
+++ example/app.js
@@ -15,17 +15,19 @@ win.open();
 var tiaudiorecorder = null;
 var audio_file = Ti.Filesystem.getFile(Ti.Filesystem.externalStorageDirectory + 'test.3gp');
   
-win.addEventListener('open', function(){
+var button = Ti.UI.createButton({title:'Record', top:0, width:200,height:60});
+button.addEventListener('click', function(){
   tiaudiorecorder = require('org.selfkleptomaniac.ti.mod.tiaudiorecorder');
   tiaudiorecorder.start();
   var new_file = tiaudiorecorder.setPath(audio_file.nativePath);
 });
+win.add(button);
 var b2 = Ti.UI.createButton({bottom:100,width:200, height:60,title:'STOP'});
 b2.addEventListener('click', function(){
   tiaudiorecorder.stop();
 }); 
 win.add(b2);
-var b3 = Ti.UI.createButton({bottom:200, width:200, height:60, title:'Record'});
+var b3 = Ti.UI.createButton({bottom:200, width:200, height:60, title:'Playback'});
 b3.addEventListener('click', function(){
   var f2 = audio_file;
   if(f2.exists()){
